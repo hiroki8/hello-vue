43e9ec3d6ad01624f20d346e553922d842f4d4da
hiroki8 <hiroki.kawano1292@gmail.com>
2020-11-04 22:33:32 +0900

    commit

--- index.js
+++ index.js
@@ -1,6 +1,97 @@
+// todo-item と呼ばれる新しいコンポーネントを定義
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+});
+
 var app = new Vue({ 
   el: '#app',
   data: {
-      message: 'Hello aaaaae!'
+      message: 'Hlo t!'
   }
 });
+var app2 = new Vue({
+  el: '#app-2',
+  data: {
+    message: 'You loaded this page on ' + new Date().toLocaleString()
+  }
+});
+var app3 = new Vue({
+  el: '#app-3',
+  data: {
+    seen: true
+  }
+});
+var app4 = new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+});
+var app5 = new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Ve.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+});
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Ve!'
+  }
+});
+var app7 = new Vue({
+  el: '#app-7',
+  data: {
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }
+})
+var vm = new Vue({
+  el: '#example',
+  data: {
+    message: 'Hello'
+  },
+  computed: {
+    // 算出 getter 関数
+    reversedMessage: function () {
+      // `this` は vm インスタンスを指します
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+var vm = new Vue({
+  el: '#demo',
+  data: {
+    firstName: 'Foo',
+    lastName: 'ar'
+  },
+  computed: {
+    fullName: {
+      // getter 関数
+      get: function () {
+        return this.firstName + ' ' + this.lastName
+      },
+      // setter 関数
+      set: function (newValue) {
+        var names = newValue.split(' ')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+})
+
+
